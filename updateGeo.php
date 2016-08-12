49bc0b851cff5c3e1519a01275489a203103a6ad
cusco <cusco@tretas.eu>
2016-08-12 09:47:06 +0100

    Added changeIp() function and some code to use it. If you want to use this make sure you understand it first Basically if you have a machine with several IP's this makes file_get_contents use a different IP when google returns OVER_QUERY_LIMIT - allowing you to bypass this limitation

--- updateGeo.php
+++ updateGeo.php
@@ -29,15 +29,46 @@ while($row = $qid->fetch_object()){
 
 function getGeo($cp4, $cp3){
 	$url = "http://maps.googleapis.com/maps/api/geocode/json?address=$cp4-$cp3&sensor=false&components=country:PT";
+	#$url = "http://ditu.google.cn//maps/api/geocode/json?address=$cp4-$cp3&sensor=false&components=country:PT";
 
-	//$data = json_decode(file_get_contents($url,false,$context));
+	/* IP CHANGING STUFF
+	 *
+	 * Uncomment this and the comment block a few lines below
+	 * only if you understand what it does, and after reading
+	 * changeIp() function comments
+	 *
+
+	$i = 0;
+	if(!file_exists('/ramdrive/googleip.txt')){
+		file_put_contents('/ramdrive/googleip.txt','10.100.100.15');
+		$currentIp = '10.100.100.16';	// default IP
+	}else{
+		$currentIp = file_get_contents('/ramdrive/googleip.txt');
+	}
+	$opts = array(
+		'socket' => array(
+			'bindto' => "$currentIp:0",
+		),
+	);
+	$context = stream_context_create($opts);
+	again:
+	$data = json_decode(file_get_contents($url,false,$context));
+	*/
+
 	$data = @file_get_contents($url,false);
 	if($data == false){
 		return "error fetching $url";
 	}
 	$data = json_decode($data);
 	if($data->status == 'OVER_QUERY_LIMIT'){
-		// todo: use another IP if available?
+		/* IP Changing stuff
+		if($i > 6){
+			die('OVER_QUERY_LIMIT');
+		}
+		$i++;
+		$context = stream_context_create(changeIp());
+		goto again:
+		*/
 		echo "Google returned over query limit\n";
 		die();
 	}
@@ -46,6 +77,54 @@ function getGeo($cp4, $cp3){
 	$geo = $data->results[$maxKey]->geometry->location;
 
 	return $geo;
+}
+
+function changeIp(){
+	/*
+	 * This will return context options for file_get_contents
+	 * the following IP list will basically be bound by the router
+	 * with a netmap rule, NATing to different external IP's
+	 * thus, allowing to increase google's limit of 2500 requests
+	 * per IP per day.
+	 *
+	 * IP List
+	 *
+	 * 10.100.101.10
+	 * 192.168.3.210
+	 * 192.168.3.211
+	 * 192.168.3.212
+	 * 192.168.3.213
+	 * 192.168.3.214
+	 */
+	$lastIp = trim(file_get_contents('/ramdrive/googleip.txt'));        // get last
+	$newIp = $lastIp; // in case for some reason it remains unchanged
+	switch($lastIp){
+		case '10.100.101.10':
+		$newIp = '192.168.3.210';
+		break;
+		case '192.168.3.210':
+		$newIp = '192.168.3.211';
+		break;
+		case '192.168.3.211':
+		$newIp = '192.168.3.212';
+		break;
+		case '192.168.3.212':
+		$newIp = '192.168.3.213';
+		break;
+		case '192.168.3.213':
+		$newIp = '192.168.3.214';
+		break;
+		case '192.168.3.214':
+		$newIp = '10.100.101.10';
+		break;
+	}
+	file_put_contents('/ramdrive/googleip.txt',$newIp);
+	$opts = array(
+		'socket' => array(
+			'bindto' => "$newIp:0",
+		),
+	);
+	return $opts;
 }
 
 ?>
